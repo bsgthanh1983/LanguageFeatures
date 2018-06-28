c5d3f8993d904992efc58fa7cae9bfd0316f84f2
bsgthanh1983 <39376025+bsgthanh1983@users.noreply.github.com>
2018-06-28 20:41:58 +0700

    bsgthanh1983 Pro ASP.NET Core MVC 2 Chapter 4 : Essential C# Features 4.3.1 - Using Auto-Implemented Property Initializers 4.3.2 - Creating Read-Only Automatically Implemented Properties

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -14,7 +14,21 @@ namespace LanguageFeatures.Controllers
                 string name = p?.Name?? "<No Name>";
                 decimal? price = p?.Price?? 0;
                 string relatedName = p?.Related?.Name?? "<None>";
-                results.Add(string.Format("Name : {0}, Price : {1}, Related : {2}", name, price, relatedName));
+                string category = p?.Category ?? "<Not decided yet>";
+                string a = "Not have information yet";
+                switch (p?.Instock)
+                {
+                    case null:
+                        a = "No Information";
+                        break;
+                    case true:
+                        a = "In stock";
+                        break;
+                    case false:
+                        a = "Out of stock";
+                        break;
+                }
+                results.Add(string.Format("Name : {0}, Price : {1}, Related : {2}, Category : {3}, Stocking State : {4}", name, price, relatedName, category,a));
             }
 
             return View(results);
--- LanguageFeatures/LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -10,21 +10,32 @@ namespace LanguageFeatures.Models
         public string Name { get; set; }
         public decimal? Price { get; set; }
         public Product Related {get;set;}
+        public string Category { get; set; } = "Watersports";
+        public bool Instock { get; } = true;
+
+        public Product(bool stock = true)
+        {
+            Instock = stock;
+        }
 
         public static Product[] GetProducts()
         {
             Product kayak = new Product
             {
                 Name = "Kayak",
-                Price = 275M
+                Price = 275M,
+                Category = "Water Craft"
+               
             };
-            Product lifejacket = new Product
+            Product lifejacket = new Product(false)
             {
                 Name = "LifeJacket",
-                Price = 48.95M
+                Price = 48.95M,
+                Related = kayak
             };
 
             kayak.Related = lifejacket;
+            
 
             return new Product[] { kayak, lifejacket, null };
         }
